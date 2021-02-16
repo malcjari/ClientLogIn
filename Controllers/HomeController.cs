58c9dbfcfc4d4895427e7892ef14b351d54c7545
hamidmalikzada <76622796+hamidmalikzada@users.noreply.github.com>
2021-02-16 15:46:20 +0100

    Update HomeController.cs

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -45,7 +45,7 @@ namespace ClientLogIn.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-            //komentarjhksjkfjskfdj
+            
         }
     }
 }
